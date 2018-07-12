b03d6d5564902aa59d05f8ef884221f97598a7aa
Loan Van Thi (Studio 7 - PHP) <loan.vt@neo-lab.vn>
2018-07-12 08:29:41 +0700

    [MIK-71] Edit Jobs table seeder

--- database/seeds/JobsTableSeeder.php
+++ database/seeds/JobsTableSeeder.php
@@ -13,7 +13,38 @@ class JobsTableSeeder extends Seeder
     {
         DB::table('jobs')->truncate();
 
-        $data = config('default_data.jobs');
+        $data = [
+            '非公開',
+            '会社員',
+            '経営者・役員',
+            '医師',
+            '弁護士',
+            '公認会計士',
+            '学生',
+            'フリーター',
+            '公務員',
+            '大手商社',
+            '外資金融',
+            '大手企業',
+            '大手外資',
+            'クリエイター',
+            'IT関連',
+            '客室乗務員',
+            '芸能・モデル',
+            '接客業',
+            'イベントコンパニオン',
+            '受付',
+            '秘書',
+            '保育士',
+            '金融',
+            'コンサル',
+            '保険',
+            '不動産',
+            '広告',
+            'マスコミ',
+            '福祉･介護',
+        ];
+
         $data = array_map(function ($name) {
             return compact('name');
         }, $data);
