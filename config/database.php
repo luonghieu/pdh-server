88254fa5dc2faaa88f07dfa71d27f4cda2bdbd8d
Bang Hoang <bang.hh@neo-lab.vn>
2018-07-12 11:38:38 +0700

    [MIK-77][DB] Config DB with different read and write endpoint

--- config/database.php
+++ config/database.php
@@ -40,8 +40,14 @@ return [
         ],
 
         'mysql' => [
+            'read' => [
+                'host' => [env('DB_READ_HOST', '127.0.0.1')],
+            ],
+            'write' => [
+                'host' => [env('DB_WRITE_HOST', '127.0.0.1')],
+            ],
+            'sticky' => true,
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
