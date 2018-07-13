3ca2702e29274a6b33380c114ce026c056a80e87
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-07-13 15:13:19 +0700

    MIK-89 update query

--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Guest;
 use App\Http\Resources\GuestResource;
+use function foo\func;
 use Illuminate\Http\Request;
 
 class GuestController extends ApiController
 {
     public function index(Request $request)
     {
-
         $guests = Guest::query();
+        $user = $this->guard()->user();
 
         if ($request->filter == 'favorited') {
-            $guests->has('favorites');
+            $guests->whereHas('favorites', function($query) use ($user) {
+                $query->where('user_id', $user->id);
+            });
         }
 
         $casts = $guests->latest()->paginate($request->per_page)->appends($request->query());
