df96d0d834a502dab05006624f328aff31cb0155
Bang Hoang <bang.hh@neo-lab.vn>
2018-07-17 10:29:07 +0700

    [MIK-114] Add is_favorited and is_blocked attribute to GuestResource and CastResource

--- app/Http/Resources/CastResource.php
+++ app/Http/Resources/CastResource.php
@@ -67,6 +67,8 @@ class CastResource extends Resource
             'working_today' => $this->working_today,
             'class_id' => $this->class_id,
             'class' => $this->class_id ? CastClass::find($this->class_id)->name : '',
+            'is_favorited' => $this->is_favorited,
+            'is_blocked' => $this->is_blocked,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ]);
