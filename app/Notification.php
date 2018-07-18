f8198e3c1e10d55afb389f84453d1735af4c4037
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-07-18 05:16:33 +0000

    feature/MIK-134 implement urban and create guest notification

--- app/Notification.php
+++ app/Notification.php
@@ -5,14 +5,5 @@ use Illuminate\Notifications\DatabaseNotification;
 
 class Notification extends DatabaseNotification
 {
-    const SEND_FROM = [
-        'user' => 1,
-        'cast' => 2,
-        'admin' => 3,
-    ];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
