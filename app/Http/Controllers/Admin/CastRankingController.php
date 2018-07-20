a197f07304a3ad30f5204ea0997ce027818bd8c8
vo tan phat <tanphatqth3@gmail.com>
2018-07-20 15:16:43 +0700

    [MIK-145][Admin][A5]キャストランキング/Cast ranking(Fix route)

--- app/Http/Controllers/Admin/CastRankingController.php
+++ app/Http/Controllers/Admin/CastRankingController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Cast_Ranking;
+namespace App\Http\Controllers\Admin;
 
 use App\Cast;
 use App\CastRanking;
--- routes/admin.php
+++ routes/admin.php
@@ -3,26 +3,33 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('login', ['as' => 'login', 'uses' => 'AuthController@index']);
     Route::post('login', ['as' => 'login', 'uses' => 'AuthController@login']);
     Route::get('logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
-    Route::group(['namespace' => 'User', 'prefix' => 'users', 'as' => 'users.', 'middleware' => 'is_admin'], function () {
-        Route::get('/', ['as' => 'index', 'uses' => 'UserController@index']);
-        Route::get('{user}', ['as' => 'show', 'uses' => 'UserController@show'])->where('user', '[0-9]+');
-        Route::put('{user}', ['as' => 'change_active', 'uses' => 'UserController@changeActive'])->where('user', '[0-9]+');
-        Route::post('{user}', ['as' => 'change_cast_class', 'uses' => 'UserController@changeCastClass'])->where('user', '[0-9]+');
+    Route::group(['namespace' => 'User', 'prefix' => 'users', 'as' => 'users.', 'middleware' => 'is_admin'],
+        function () {
+            Route::get('/', ['as' => 'index', 'uses' => 'UserController@index']);
+            Route::get('{user}', ['as' => 'show', 'uses' => 'UserController@show'])->where('user', '[0-9]+');
+            Route::put('{user}', ['as' => 'change_active', 'uses' => 'UserController@changeActive'])->where('user',
+                '[0-9]+');
+            Route::post('{user}',
+                ['as' => 'change_cast_class', 'uses' => 'UserController@changeCastClass'])->where('user', '[0-9]+');
+        });
+
+    Route::group(['namespace' => 'Cast', 'prefix' => 'casts', 'as' => 'casts.', 'middleware' => 'is_admin'],
+        function () {
+            Route::get('/', ['as' => 'index', 'uses' => 'CastController@index']);
+            Route::get('{user}/register', ['as' => 'register', 'uses' => 'CastController@registerCast']);
+            Route::post('{user}/confirm', ['as' => 'confirm', 'uses' => 'CastController@confirmRegister']);
+            Route::post('{user}/save', ['as' => 'save', 'uses' => 'CastController@saveCast']);
+        });
+
+    Route::group(['middleware' => 'is_admin'], function () {
+        Route::get('cast_rankings', ['as' => 'cast_rankings.index', 'uses' => 'CastRankingController@index']);
     });
 
-    Route::group(['namespace' => 'Cast', 'prefix' => 'casts', 'as' => 'casts.', 'middleware' => 'is_admin'], function () {
-        Route::get('/', ['as' => 'index', 'uses' => 'CastController@index']);
-        Route::get('{user}/register', ['as' => 'register', 'uses' => 'CastController@registerCast']);
-        Route::post('{user}/confirm', ['as' => 'confirm', 'uses' => 'CastController@confirmRegister']);
-        Route::post('{user}/save', ['as' => 'save', 'uses' => 'CastController@saveCast']);
-    });
-
-    Route::group(['namespace' => 'Cast_Ranking', 'prefix' => 'cast_rankings', 'as' => 'cast_rankings.', 'middleware' => 'is_admin'], function () {
-        Route::get('/', ['as' => 'index', 'uses' => 'CastRankingController@index']);
-    });
-
-    Route::group(['namespace' => 'Room', 'prefix' => 'rooms', 'as' => 'rooms.', 'middleware' => 'is_admin'], function () {
-        Route::get('{room}/messages', ['as' => 'messages_by_room', 'uses' => 'RoomController@getMessageByRoom'])->where('room', '[0-9]+');
-        Route::put('{room}', ['as' => 'change_active', 'uses' => 'RoomController@changeActive'])->where('room', '[0-9]+');
-    });
+    Route::group(['namespace' => 'Room', 'prefix' => 'rooms', 'as' => 'rooms.', 'middleware' => 'is_admin'],
+        function () {
+            Route::get('{room}/messages',
+                ['as' => 'messages_by_room', 'uses' => 'RoomController@getMessageByRoom'])->where('room', '[0-9]+');
+            Route::put('{room}', ['as' => 'change_active', 'uses' => 'RoomController@changeActive'])->where('room',
+                '[0-9]+');
+        });
 });
