90b826c57d14db5d7dc76028feae732afc70f759
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-07-20 16:40:49 +0700

    [MIK-171][API][UPDATE] Post message

---
+++ app/Jobs/MakeImagesChatThumbnail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Webpatser\Uuid\Uuid;
+
+class MakeImagesChatThumbnail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $info = pathinfo($this->message->image);
+        $contents = file_get_contents($this->message->image);
+        $imageName = Uuid::generate()->string . '.' . strtolower($info['extension']);
+        $image = \Image::make($contents)->resize(500, null)->encode($info['extension']);
+
+        \Storage::put($imageName, $image->__toString(), 'public');
+        $this->message->update([
+            'thumbnail' => $imageName,
+        ]);
+    }
+}
