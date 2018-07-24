99f4afb82f7a46d48090d2c384943a532714c16a
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-07-24 14:43:55 +0700

    feature/MIK-206 notification when cast deny nomination orders

---
+++ app/Notifications/CastDenyNominationOrders.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Enums\MessageType;
+use App\Enums\RoomType;
+use App\Enums\UserType;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CastDenyNominationOrders extends Notification
+{
+    use Queueable;
+
+    public $order;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $order
+     */
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [CustomDatabaseChannel::class, PushNotificationChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     */
+    public function toMail($notifiable)
+    {
+        return;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        $order = $this->order;
+        $room = $order->room;
+
+        if ($notifiable->type == UserType::CAST) {
+            $castMessage = '提案がキャンセルされました。';
+
+            $roomMessage = $room->messages()->create([
+                'user_id' => 1,
+                'type' => MessageType::SYSTEM,
+                'message' => $castMessage
+            ]);
+
+            $roomMessage->recipients()->attach($notifiable->id, ['room_id' => $room->id, 'is_show' => false]);
+        }
+
+        $message = '残念ながらマッチングが成立しませんでした（；；）';
+
+        $roomMessage = $room->messages()->create([
+            'user_id' => 1,
+            'type' => MessageType::SYSTEM,
+            'message' => $message
+        ]);
+
+        $roomMessage->recipients()->attach($notifiable->id, ['room_id' => $room->id, 'is_show' => false]);
+
+        return [
+            'content' => $message,
+            'send_from' => UserType::ADMIN,
+        ];
+    }
+
+    public function pushData($notifiable)
+    {
+        $content = '残念ながらマッチングが成立しませんでした（；；）';
+
+        $namedUser = 'user_' . $notifiable->id;
+        $send_from = UserType::ADMIN;
+        $pushId = ($notifiable->type == UserType::GUEST) ? 'g_9' : 'c_9';
+
+        return [
+            'audienceOptions' => ['named_user' => $namedUser],
+            'notificationOptions' => [
+                'alert' => $content,
+                'ios' => [
+                    'alert' => $content,
+                    'sound' => 'cat.caf',
+                    'badge' => '+1',
+                    'content-available' => true,
+                    'extra' => [
+                        'push_id' => $pushId,
+                        'send_from' => $send_from,
+                        'order_id' => $this->order->id
+                    ],
+                ],
+            ],
+        ];
+    }
+}
