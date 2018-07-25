3e1cc637ebd01676acde05808a5b0d299d8c8ab4
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-07-25 16:59:39 +0700

    feature/MIK-184 change class name

--- app/Notifications/StartOrder.php
+++ app/Notifications/StartOrder.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class OrderStarted extends Notification
+class StartOrder extends Notification
 {
     use Queueable;
 
