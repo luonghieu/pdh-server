da1532c9c3476dc65fe8c710a0081a6fe2707c79
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-07-27 14:12:09 +0700

    [MIK-225][UPDATE] Cast stop order

--- app/Order.php
+++ app/Order.php
@@ -50,7 +50,7 @@ class Order extends Model
             ->where('cast_order.type', CastOrderType::CANDIDATE)->withTimestamps();
     }
 
-    public function processing()
+    public function castsProcessing()
     {
         return $this->belongsToMany(Cast::class)
             ->where('cast_order.status', CastOrderStatus::PROCESSING)->withTimestamps();
