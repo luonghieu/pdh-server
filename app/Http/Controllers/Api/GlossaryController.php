d2eedfaa0d859dbe2b95a21b0984420b5dd74313
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-07-30 16:53:35 +0700

    feature/MIK-236 caculate cast point

--- app/Http/Controllers/Api/GlossaryController.php
+++ app/Http/Controllers/Api/GlossaryController.php
@@ -20,8 +20,6 @@ class GlossaryController extends ApiController
 {
     public function glossary()
     {
-        $order = Order::find(7);
-        return $this->caculatePoint($order, 3);
         $drinkVolumes = [];
         $smokings = [];
         $siblings = [];
@@ -70,69 +68,5 @@ class GlossaryController extends ApiController
         $data['jobs'] = Job::all(['id', 'name'])->toArray();
 
         return $this->respondWithData($data);
-    }
-
-    private function caculatePoint($order, $castId) {
-        $nightAllowance = 0;
-        $extralTime = 0;
-
-        $cast = $order->belongsToMany(Cast::class)->where('cast_order.status', CastOrderStatus::PROCESSING)
-            ->withTimestamps()->withPivot('started_at', 'stopped_at')->where('user_id', $castId)->first();
-
-        $startDate = Carbon::parse($order->date . ' ' .$order->start_time);
-        $endDate = $startDate->copy()->addHours($order->duration);
-
-        $orderHasAllowance = $this->isAllowanceOrder($startDate, $endDate);
-        if ($orderHasAllowance) {
-            $nightAllowance = 4000;
-        }
-
-        $castStoppedAt = Carbon::parse($cast->pivot->stopped_at);
-        if ($castStoppedAt > $endDate) {
-            $extralTime = $castStoppedAt->diffInMinutes($endDate);
-        }
-
-        $extraCount = 0;
-        if ($extralTime > 15) {
-            dump($extralTime);
-            while ($extralTime >= 16) {
-                $extraCount++;
-                $extralTime = $extralTime - 16;
-                dump($extralTime);
-            }
-        }
-
-        dd($extraCount);
-        dd('------------------------');
-    }
-
-    private function isAllowanceOrder($startDate, $endDate)
-    {
-        $allowanceStartTime = Carbon::parse('00:01:00');
-        $allowanceEndTime = Carbon::parse('04:00:00');
-
-        $startDay = Carbon::parse($startDate)->startOfDay();
-        $endDay = Carbon::parse($endDate)->startOfDay();
-
-        $timeStart = Carbon::parse(Carbon::parse($startDate->format('H:i:s')));
-        $timeEnd = Carbon::parse(Carbon::parse($endDate->format('H:i:s')));
-
-        if ($startDay->diffInDays($endDay) != 0 && !$endDate->eq($endDate->copy()->startOfDay())) {
-            return true;
-        }
-
-        if ($timeStart->between($allowanceStartTime, $allowanceEndTime) || $timeEnd->between($allowanceStartTime, $allowanceEndTime)) {
-            return true;
-        }
-
-        if ($timeStart < $allowanceStartTime  && $timeEnd > $allowanceEndTime) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function hasExtraTime($castOrders) {
-
     }
 }
