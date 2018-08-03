cbe15f6a6025c2171debd18447a0de7574c06c8f
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-08-03 15:19:05 +0700

    [MIK-303] Cast delete order

--- app/Http/Controllers/Api/Cast/OrderController.php
+++ app/Http/Controllers/Api/Cast/OrderController.php
@@ -279,6 +279,14 @@ class OrderController extends ApiController
             CastOrderStatus::TIMEOUT,
         ];
         $user = $this->guard()->user();
+
+        $castExists = $order->castOrder()->where('cast_order.user_id', $user->id)
+            ->where('cast_order.order_id', $id)->exists();
+
+        if (!$castExists) {
+            return $this->respondErrorMessage(trans('messages.action_not_performed'), 422);
+        }
+
         try {
             $order->castOrder()->where('cast_order.user_id', $user->id)
                 ->where('cast_order.order_id', $id)
@@ -287,10 +295,9 @@ class OrderController extends ApiController
 
             return $this->respondWithNoData(trans('messages.delete_order_success'));
         } catch (\Exception $e) {
-            DB::rollBack();
             LogService::writeErrorLog($e);
 
             return $this->respondServerError();
         }
     }
-};
+}
