799174a0fcac8602b67ec4490ca5e36b8fa59635
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-08-04 14:40:39 +0700

    [MIK-324] [API] - Create receipt (update)

--- database/migrations/2018_08_01_190653_alter_table_receipt_change_field_address_to_name.php
+++ database/migrations/2018_08_01_190653_alter_table_receipt_change_field_address_to_name.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AlterTableReceiptChangeFieldAddressToName extends Migration
 {
@@ -14,7 +14,7 @@ class AlterTableReceiptChangeFieldAddressToName extends Migration
     public function up()
     {
         Schema::table('receipts', function (Blueprint $table) {
-            $table->string('name')->after('date');
+            $table->string('name')->nullable()->after('date');
             $table->dropColumn('address');
         });
     }
