63dc2b42e5ec4a120721ccc91a3a2884423702a3
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-08-06 11:44:27 +0700

    feature/MIK-333 fix  admin email

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-//        DB::table('users')->truncate();
+        DB::table('users')->truncate();
 
         $faker = Faker\Factory::create();
 
