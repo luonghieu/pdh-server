cc96d5eb469b86b5a2e7b2343d459cf16d08ea55
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-08-10 17:39:37 +0700

    [MIK-467] Return for cast 80% points in payment requests

--- app/Http/Resources/PaymentRequestResource.php
+++ app/Http/Resources/PaymentRequestResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Traits\ResourceResponse;
+use Auth;
 use Illuminate\Http\Resources\Json\Resource;
 
 class PaymentRequestResource extends Resource
@@ -17,6 +18,8 @@ class PaymentRequestResource extends Resource
      */
     public function toArray($request)
     {
+        $isCast = Auth::user()->is_cast;
+
         return $this->filterNull([
             'id' => $this->id,
             'cast_id' => $this->cast_id,
@@ -24,11 +27,11 @@ class PaymentRequestResource extends Resource
             'order_id' => $this->order_id,
             'order_time' => $this->order_time,
             'extra_time' => $this->extra_time,
-            'order_point' => $this->order_point,
-            'extra_point' => $this->extra_point,
-            'allowance_point' => $this->allowance_point,
-            'fee_point' => $this->fee_point,
-            'total_point' => $this->total_point,
+            'order_point' => $this->when($isCast, 0.8 * $this->order_point, $this->order_point),
+            'extra_point' => $this->when($isCast, 0.8 * $this->extra_point, $this->extra_point),
+            'allowance_point' => $this->when($isCast, 0.8 * $this->allowance_point, $this->allowance_point),
+            'fee_point' => $this->when($isCast, 0.8 * $this->fee_point, $this->fee_point),
+            'total_point' => $this->when($isCast, 0.8 * $this->total_point, $this->total_point),
             'status' => $this->status,
             'cast' => CastResource::make($this->whenLoaded('cast')),
             'order' => OrderResource::make($this->whenLoaded('order')),
