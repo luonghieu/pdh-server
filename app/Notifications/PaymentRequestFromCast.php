0b6cdba53676b1391ce6118962bc5b2300f636cc
Huynh Thi Hoai Thuong <thuong.hth@neo-lab.vn>
2018-08-13 14:57:06 +0700

    [MIK-456] update nickname user in  message

--- app/Notifications/PaymentRequestFromCast.php
+++ app/Notifications/PaymentRequestFromCast.php
@@ -8,9 +8,8 @@ use App\Enums\SystemMessageType;
 use App\Enums\UserType;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
 
 class PaymentRequestFromCast extends Notification implements ShouldQueue
 {
@@ -66,14 +65,15 @@ class PaymentRequestFromCast extends Notification implements ShouldQueue
     {
         $orderStartDate = Carbon::parse($this->order->date . ' ' . $this->order->start_time);
         $orderEndDate = Carbon::parse($this->order->actual_ended_at);
+        $guestNickname = $this->order->user->nickname ? $this->order->user->nickname . '様' : 'お客様';
 
         $content = 'Cheersをご利用いただきありがとうございました♪'
-            . PHP_EOL . $orderStartDate->format('Y/m/d H:i') . '~' . $orderEndDate->format('H:i') . 'の合計ポイントは' .
-            $this->order->total_point . 'Pointです。'
+        . PHP_EOL . $orderStartDate->format('Y/m/d H:i') . '~' . $orderEndDate->format('H:i') . 'の合計ポイントは' .
+        $this->order->total_point . 'Pointです。'
             . PHP_EOL . '合計ポイントの詳細はコチラから確認することができます。'
             . PHP_EOL . '※詳細に誤りがある場合は、24時間以内に「決済ポイントの修正依頼をする」を押してください。運営から確認のご連絡を差し上げます。'
             . PHP_EOL . PHP_EOL . 'ご不明点がございましたらいつでもお問い合わせください。'
-            . PHP_EOL . PHP_EOL . '○○様のまたのご利用をお待ちしております♪';
+            . PHP_EOL . PHP_EOL . $guestNickname . 'またのご利用をお待ちしております♪';
 
         $room = $notifiable->rooms()
             ->where('rooms.type', RoomType::SYSTEM)
@@ -83,7 +83,7 @@ class PaymentRequestFromCast extends Notification implements ShouldQueue
             'type' => MessageType::SYSTEM,
             'message' => $content,
             'system_type' => SystemMessageType::NORMAL,
-            'order_id' => $this->order->id
+            'order_id' => $this->order->id,
         ]);
         $roomMessage->recipients()->attach($notifiable->id, ['room_id' => $room->id]);
 
@@ -103,7 +103,7 @@ class PaymentRequestFromCast extends Notification implements ShouldQueue
                     'extra' => [
                         'push_id' => $pushId,
                         'send_from' => $send_from,
-                        'order_id' => $this->order->id
+                        'order_id' => $this->order->id,
                     ],
                 ],
             ],
