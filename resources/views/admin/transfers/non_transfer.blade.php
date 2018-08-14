bbdcff99cf1d093422fe1b233fc6c456d77bce35
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-08-14 17:09:42 +0700

    [MIK-414] [UPDATE] [Admin] - [A11]振込管理_未振込quản lý doanh thu_chưa chuyển khoản

--- resources/views/admin/transfers/non_transfer.blade.php
+++ resources/views/admin/transfers/non_transfer.blade.php
@@ -75,7 +75,7 @@
                   <td>{{ Carbon\Carbon::parse($transfer->created_at)->format('Y年m月d日') }}</td>
                   <td>{{ $transfer->user_id }}</td>
                   <td>{{ $transfer->user->fullname }}</td>
-                  <td>￥{{ $transfer->amount }}</td>
+                  <td>￥{{ number_format($transfer->amount) }}</td>
                   @if($transfer->order)
                     @if (App\Enums\OrderType::NOMINATION == $transfer->order->type)
                     <td>
@@ -98,7 +98,7 @@
                   <td></td>
                   <td></td>
                   <td></td>
-                  <td>¥ {{ $transfers->sum('amount') }}</td>
+                  <td>¥ {{ number_format($transfers->sum('amount')) }}</td>
                   <td></td>
                 </tr>
               @else
