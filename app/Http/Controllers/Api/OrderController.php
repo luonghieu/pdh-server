f8677ef4525d64db2c414cfa00b835125f402b96
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-08-16 10:58:38 +0700

    feature/MIK-531 update cast_order cost field

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -98,9 +98,9 @@ class OrderController extends ApiController
             }
         }
 
-        if ($orders->count() > 0 || $listCastMatching > 0) {
-            return $this->respondErrorMessage(trans('messages.order_same_time'), 409);
-        }
+//        if ($orders->count() > 0 || $listCastMatching > 0) {
+//            return $this->respondErrorMessage(trans('messages.order_same_time'), 409);
+//        }
 
         if (!$user->cards->first()) {
             return $this->respondErrorMessage(trans('messages.card_not_exist'), 404);
@@ -144,6 +144,14 @@ class OrderController extends ApiController
                     $order->save();
 
                     if ($order->type == OrderType::NOMINATION) {
+                        $order->nominees()->updateExistingPivot(
+                            $nominee->id,
+                            [
+                                'cost' => $nominee->cost,
+                            ],
+                            false
+                        );
+
                         $nominee->notify(new CreateNominationOrdersForCast($order));
                     }
                 }
