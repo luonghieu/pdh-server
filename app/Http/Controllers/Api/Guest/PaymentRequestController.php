b9892cd8acbe40c323f7034719e3ee5ee4f39d31
Bang Hoang <bang.hh@neo-lab.vn>
2018-08-18 23:04:39 +0700

    [MIK-549] Get payment requests for guest when order is canceled

--- app/Http/Controllers/Api/Guest/PaymentRequestController.php
+++ app/Http/Controllers/Api/Guest/PaymentRequestController.php
@@ -45,7 +45,7 @@ class PaymentRequestController extends ApiController
             return $this->respondErrorMessage(trans('messages.order_not_found'), 404);
         }
 
-        if (OrderStatus::DONE != $order->status || !$order->payment_status || !$order->paymentRequests->count()) {
+        if (OrderStatus::DONE != $order->status || OrderStatus::CANCELED != $order->status || !$order->payment_status || !$order->paymentRequests->count()) {
             return $this->respondErrorMessage(trans('messages.action_not_performed'), 422);
         }
 
