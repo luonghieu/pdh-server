a9da2f1fb169761c0aaa9cdd73f9c95062acdc88
Huynh Thi Hoai Thuong <thuong.hth@neo-lab.vn>
2018-09-07 10:56:54 +0700

    [MIK-618] Update

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,13 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Webview', 'prefix' => 'webview', 'as' => 'webview.'], function () {
+    Route::get('card/create', ['as' => 'create', 'uses' => 'CreditCardController@create']);
+    Route::group(['middleware' => ['auth', 'guest']], function () {
+        Route::post('card/create', ['as' => 'add_card', 'uses' => 'CreditCardController@addCard']);
+        Route::get('card/{card}', ['as' => 'show', 'uses' => 'CreditCardController@show'])->where('card', '[0-9]+');
+        Route::get('card/edit/{card}', ['as' => 'edit', 'uses' => 'CreditCardController@edit'])->where('card', '[0-9]+');
+    });
 });
-
