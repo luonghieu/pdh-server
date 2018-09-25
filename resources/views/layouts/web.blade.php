776deafe806336f7d30116c381ced4eb76a8ed1f
Bang Hoang <bang.hh@neo-lab.vn>
2018-09-25 13:37:32 +0700

    Update viewport

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="utf-8">
   <title>@yield('title')</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta name="keywords" content="">
   <meta name="description" content="">
   <meta name="csrf-token" content="{{ csrf_token() }}">
