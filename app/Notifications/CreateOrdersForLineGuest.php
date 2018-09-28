7fe438d4a6fd26f49eb4a9aabb7151f9f05fba98
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-09-28 09:48:12 +0700

    feature/MIK-820 fix display text in create order for LINE guest

--- app/Notifications/CreateOrdersForLineGuest.php
+++ app/Notifications/CreateOrdersForLineGuest.php
@@ -3,6 +3,7 @@
 namespace App\Notifications;
 
 use App\Enums\MessageType;
+use App\Enums\OrderType;
 use App\Enums\RoomType;
 use App\Enums\SystemMessageType;
 use App\Order;
@@ -89,21 +90,37 @@ class CreateOrdersForLineGuest extends Notification implements ShouldQueue
         ]);
         $roomMessage->recipients()->attach($notifiable->id, ['room_id' => $room->id]);
 
-        $content = 'Cheersをご利用いただきありがとうございます！✨'
-            . PHP_EOL . 'キャストのご予約を承りました。'
-            . PHP_EOL . '--------------------------------------'
-            . PHP_EOL . '▼ご予約内容'
-            . PHP_EOL . '日時：' . $startTime->format('Y/m/d H:i') . '~'
-            . PHP_EOL . '時間：' . $startTime->diffInMinutes($endTime) / 60 . '時間'
-            . PHP_EOL . 'クラス：' . $this->order->castClass->name
-            . PHP_EOL . '人数：' . $this->order->total_cast . '人'
-            . PHP_EOL . '場所：' . $this->order->address
-            . PHP_EOL . PHP_EOL . '現在、キャストの調整を行っております。'
-            . PHP_EOL . 'しばらくお待ちください☆'
-            . PHP_EOL .  PHP_EOL . '【このあとの流れ】'
-            . PHP_EOL .  '①キャストが揃うと、マッチング成功'
-            . PHP_EOL .  '②開催予定のお店のURLと予約名を送信'
-            . PHP_EOL .  '③マッチング終了後、評価と決済！';
+        if ($this->order->type == OrderType::NOMINATION) {
+            $content = 'Cheersをご利用いただきありがとうございます！✨'
+                . PHP_EOL . 'キャストのご予約を承りました。'
+                . PHP_EOL . '--------------------------------------'
+                . PHP_EOL . '▼ご予約内容'
+                . PHP_EOL . '日時：' . $startTime->format('Y/m/d H:i') . '~'
+                . PHP_EOL . '時間：' . $startTime->diffInMinutes($endTime) / 60 . '時間'
+                . PHP_EOL . '場所：' . $this->order->address
+                . PHP_EOL . PHP_EOL . '現在、キャストの調整を行っております。'
+                . PHP_EOL . 'しばらくお待ちください☆'
+                . PHP_EOL .  PHP_EOL . '【このあとの流れ】'
+                . PHP_EOL .  '①キャストが揃うと、マッチング成功'
+                . PHP_EOL .  '②開催予定のお店のURLと予約名を送信'
+                . PHP_EOL .  '③マッチング終了後、評価と決済！';
+        } else {
+            $content = 'Cheersをご利用いただきありがとうございます！✨'
+                . PHP_EOL . 'キャストのご予約を承りました。'
+                . PHP_EOL . '--------------------------------------'
+                . PHP_EOL . '▼ご予約内容'
+                . PHP_EOL . '日時：' . $startTime->format('Y/m/d H:i') . '~'
+                . PHP_EOL . '時間：' . $startTime->diffInMinutes($endTime) / 60 . '時間'
+                . PHP_EOL . 'クラス：' . $this->order->castClass->name
+                . PHP_EOL . '人数：' . $this->order->total_cast . '人'
+                . PHP_EOL . '場所：' . $this->order->address
+                . PHP_EOL . PHP_EOL . '現在、キャストの調整を行っております。'
+                . PHP_EOL . 'しばらくお待ちください☆'
+                . PHP_EOL .  PHP_EOL . '【このあとの流れ】'
+                . PHP_EOL .  '①キャストが揃うと、マッチング成功'
+                . PHP_EOL .  '②開催予定のお店のURLと予約名を送信'
+                . PHP_EOL .  '③マッチング終了後、評価と決済！';
+        }
 
         return [
             [
