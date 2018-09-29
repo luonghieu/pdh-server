ac4aea21d3b746359865594cf100cc384a1a498e
Bang Hoang <bang.hh@neo-lab.vn>
2018-09-29 11:23:56 +0700

    Set a fixed domain for web routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -53,7 +53,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
+        Route::domain(env('APP_URL'))
+            ->middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
     }
