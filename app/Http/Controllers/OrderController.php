e118a1f3ef532dd05566681f8ab82a197078deb3
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-10-01 17:33:10 +0700

    feature/MIK-906 fix payment bug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -558,7 +558,6 @@ class OrderController extends Controller
         try {
             DB::beginTransaction();
             if (!$order->settle()) {
-                DB::commit();
                 return response()->json(['success' => false], 500);
             }
 
