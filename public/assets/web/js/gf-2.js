76e72908df133b629ff92857a43c19867ff5e590
lisakishi <risa.kishi@neo-lab.co.jp>
2018-10-02 18:42:21 +0900

    Modify the layout of each page

--- public/assets/web/js/gf-2.js
+++ public/assets/web/js/gf-2.js
@@ -2,7 +2,7 @@ $(function(){
   $('.cast-photo__show').slick({
     dots:true,
     customPaging: function(slick,index) {
-        slick.$slides.eq(index).css("background","red")
+        slick.$slides.eq(index).css("background","white")
         var targetImage = slick.$slides.eq(index).attr('src');
         return '<img src=' + targetImage +'>';
     }
