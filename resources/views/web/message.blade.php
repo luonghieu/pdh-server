b7300863a9dbc5a507cacc7f1814672ec0f29eda
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-10-04 13:52:43 +0700

    [MIK-928] [Web] Update create order validation.

--- resources/views/web/message.blade.php
+++ resources/views/web/message.blade.php
@@ -187,5 +187,9 @@
   if(localStorage.getItem("order_params")){
     localStorage.removeItem("order_params");
   }
+
+  if(localStorage.getItem("back_link")){
+    localStorage.removeItem("back_link");
+  }
 </script>
 @stop
