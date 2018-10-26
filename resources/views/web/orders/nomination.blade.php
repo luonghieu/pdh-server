b458f578fc81407253935daf53878e31e7281dff
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-10-26 15:24:51 +0700

    [MIK-1050] Refactor code

--- resources/views/web/orders/nomination.blade.php
+++ resources/views/web/orders/nomination.blade.php
@@ -256,11 +256,11 @@
     @modal(['triggerId' => 'md-require-card', 'triggerClass' =>'lable-register-card','button' =>'クレジットカードを登録する
 '])
       @slot('title')
-        クレジットカードを 登録してください
+        クレジットカードを 登録<br>してください
       @endslot
 
       @slot('content')
-      ※キャストと合流するまで 料金は発生しません
+      ※キャストと合流するまで 料金は<br>発生しません
       @endslot
     @endmodal
   @else
