839eb9092dffc88e46c2a30edde7a0c30656c140
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-10-30 16:21:52 +0700

    feature/MIK-1065 update admin notification

--- app/Enums/DeviceType.php
+++ app/Enums/DeviceType.php
@@ -9,17 +9,4 @@ final class DeviceType extends Enum
     const IOS = 1;
     const ANDROID = 2;
     const WEB = 3;
-
-    public static function getDescription($value): string
-    {
-        if (self::WEB == $value) {
-            return 'Web';
-        } elseif (self::ANDROID == $value) {
-            return 'Android';
-        } elseif (self::IOS == $value) {
-            return 'IOS';
-        }
-
-        return parent::getDescription($value);
-    }
 }
