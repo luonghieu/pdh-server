fd37c965748b6df7a47858633cf0861b090ae55d
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-10-30 15:32:47 +0700

    [MIK-1032] Update url for cast my page

--- routes/web.php
+++ routes/web.php
@@ -103,7 +103,7 @@ Route::group(['middleware' => ['auth', 'guest', 'check_info']], function () {
 });
 
 Route::group(['middleware' => ['auth', 'cast', 'check_info'], 'as' => 'cast.', 'prefix' => 'cast_mypage'], function () {
-    Route::group(['middleware' => 'auth'], function () {
+    Route::group([], function () {
         Route::get('/transfer_history', ['as' => 'transfer_history', 'uses' => 'PaymentController@history']);
         Route::get('/transfer_history/load_more', ['as' => 'transfer_history_load_more', 'uses' => 'PaymentController@loadMore']);
     });
