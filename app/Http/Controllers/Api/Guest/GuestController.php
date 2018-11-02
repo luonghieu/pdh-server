d0be1f0699bdee424c916a4b3314516651e3a484
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-11-02 12:01:24 +0700

    feature/MIK-1096 add living_id to request transfer to cast api

--- app/Http/Controllers/Api/Guest/GuestController.php
+++ app/Http/Controllers/Api/Guest/GuestController.php
@@ -89,7 +89,8 @@ class GuestController extends ApiController
             'job_id' => 'numeric|exists:jobs,id|required',
             'line_qr' => 'image|mimes:jpeg,png,jpg,gif,svg|max:5120',
             'images' => 'array|required|min:2|max:2',
-            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:5120'
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:5120',
+            'living_id' => 'required|numeric|exists:prefectures,id',
         ];
 
         $validator = validator($request->all(), $rules);
@@ -129,6 +130,7 @@ class GuestController extends ApiController
             $user->type = UserType::CAST;
             $user->cast_transfer_status = CastTransferStatus::PENDING;
             $user->request_transfer_date = Carbon::now();
+            $user->living_id = $request->living_id;
 
             $user->save();
             \DB::commit();
