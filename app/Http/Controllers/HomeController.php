4fb73dcf1c63dc111f14789c4ab789bafcd52c21
Bang Hoang <bang.hh@neo-lab.vn>
2018-11-05 15:41:39 +0700

    Fix bug guest mypage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,35 +44,22 @@ class HomeController extends Controller
                     ->with(['user', 'casts', 'nominees', 'tags'])
                     ->orderBy('date')
                     ->orderBy('start_time')->first();
-
-                if (!$order) {
-                    $order = Order::with('casts')
-                        ->where('user_id', Auth::user()->id)
-                        ->whereIn('status', [OrderStatus::OPEN, OrderStatus::ACTIVE])
-                        ->with(['user', 'casts', 'nominees', 'tags'])
-                        ->orderBy('date')
-                        ->orderBy('start_time')->first();
-                }
-
-                $order = OrderResource::make($order);
-
-                $client = new Client(['base_uri' => config('common.api_url')]);
-                $option = [
-                    'headers' => ['Authorization' => 'Bearer ' . $token],
-                    'form_params' => [],
-                    'allow_redirects' => false,
-                ];
-
-                $response = $client->get(route('casts.index', ['working_today' => 1, 'device' => 3]), $option);
-                $getContents = json_decode($response->getBody()->getContents());
-                $casts = $getContents->data;
-
-                return view('web.index', compact('token', 'order', 'casts'));
             }
 
             $order = OrderResource::make($order);
 
-            return view('web.index', compact('token', 'order'));
+            $client = new Client(['base_uri' => config('common.api_url')]);
+            $option = [
+                'headers' => ['Authorization' => 'Bearer ' . $token],
+                'form_params' => [],
+                'allow_redirects' => false,
+            ];
+
+            $response = $client->get(route('casts.index', ['working_today' => 1, 'device' => 3]), $option);
+            $getContents = json_decode($response->getBody()->getContents());
+            $casts = $getContents->data;
+
+            return view('web.index', compact('token', 'order', 'casts'));
         }
 
         return redirect()->route('web.login');
