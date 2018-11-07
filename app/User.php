d7cce7fa79dd4bfa2e312428981fa508429f5edc
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-11-07 14:36:18 +0700

    feature/MIK-1135 add soft delete to user

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use App\Enums\PointType;
 use App\Enums\UserType;
 use App\Services\LogService;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
@@ -16,7 +17,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     protected $hidden = [
         'password',
