b990cd1461f9a353fc6a9045e2cce64e7b4e721b
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-11-10 17:19:17 +0700

    [MIK-1151] [Web] - Show popup notice to add LINE friend on my page.

--- resources/assets/js/web/pages/index.js
+++ resources/assets/js/web/pages/index.js
@@ -1,1 +1,12 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  const helper = require('./helper');
+  $('#cookie-link').on('click', function (e) {
+    var checked = $('#input-cookie:checked').length;
+
+    if(checked) {
+      if(!getCookie('popup')) {
+        helper.setCookie('popup',1);
+      }
+    }
+  });
+});
