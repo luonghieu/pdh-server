1284f681bd07a259e55683f0b7f1cd3380a6e44d
Pham Dinh Hung (Studio 7- PHP) <hung.pd@neo-lab.vn>
2018-11-10 17:19:17 +0700

    [MIK-1151] [Web] - Show popup notice to add LINE friend on my page.

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -37,6 +37,34 @@
         </div>
       </div>
   </div>
+
+  @if (Auth::check())
+    @if(App\Enums\UserType::GUEST == Auth::user()->type)
+    <div class="modal_wrap">
+      <input id="cookie-popup" type="checkbox">
+      <div class="modal_overlay modal_overlay-popup">
+          <label for="cookie-popup" class="modal_trigger"></label>
+          <div class="modal_content modal_content-btn2">
+              <div class="text-box">
+                  <h2 id="" style="color: #d0021b">重要なお知らせ</h2>
+                  <p>現在、LINE@の不具合により、皆様の<br/>LINEに通知が届いておりません<br>
+                  ご面倒をおかけしますが、下記のLINE@に<br>再度友だち登録をしていただきたいです<br>
+                  <br>
+                  キャストからのメッセージも届かない状況<br>ですので、何卒よろしくお願い致します
+                  </p>
+                  <div>
+                  <input type="checkbox" id="input-cookie" style="display: inline-block;margin-bottom: 15px;margin-bottom: 15px;margin-right: 15px;">今後表示しない
+                  </div>
+              </div>
+              <div class="close_button-box">
+                  <label for="cookie-popup" class="close_button">
+                    <a href="{{ 'https://line.me/R/ti/p/%40' . env('LINE_ID') }}" style="color: #FF66BB" id="cookie-link">友だち登録する</a></label>
+              </div>
+          </div>
+      </div>
+    </div>
+    @endif
+  @endif
 @endsection
 @section('web.content')
   @if (!Auth::check())
@@ -193,6 +221,28 @@
 
     if(localStorage.getItem("back_link")){
       localStorage.removeItem("back_link");
+    }
+
+    function getCookie(cookie_name) {
+      if (document.cookie.length > 0) {
+          var cookie_start = document.cookie.indexOf(cookie_name + "=");
+          if (cookie_start != -1) {
+              cookie_start = cookie_start + cookie_name.length + 1;
+              var cookie_end = document.cookie.indexOf(";", cookie_start);
+              if (cookie_end == -1) {
+                  cookie_end = document.cookie.length;
+              }
+              return unescape(document.cookie.substring(cookie_start, cookie_end));
+          }
+      }
+
+      return "";
+    }
+
+    if(!getCookie('popup')) {
+      $('#cookie-popup').click();
+    }else {
+     $('.modal_overlay-popup').css('display','none');
     }
 
   </script>
