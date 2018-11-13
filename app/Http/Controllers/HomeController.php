fdad17cc6c91936029916a00d8e197383211e2e9
Bang Hoang <bang.hh@neo-lab.vn>
2018-11-13 09:36:45 +0700

    Pick

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Enums\OrderStatus;
+use App\Enums\UserType;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
 use App\Order;
@@ -10,7 +11,6 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Session;
-use File;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class HomeController extends Controller
@@ -26,8 +26,9 @@ class HomeController extends Controller
             $request->session()->forget('data');
         }
         if (Auth::check()) {
+            $user = Auth::user();
             $token = '';
-            $token = JWTAuth::fromUser(Auth::user());
+            $token = JWTAuth::fromUser($user);
 
             if (!$user->status) {
                 return view('web.users.verification', compact('token'));
@@ -36,26 +37,56 @@ class HomeController extends Controller
             if (UserType::GUEST == $user->type) {
                 $order = Order::with('casts')
                     ->where('user_id', Auth::user()->id)
-                    ->whereIn('status', [OrderStatus::OPEN, OrderStatus::ACTIVE])
+                    ->where('status', OrderStatus::PROCESSING)
                     ->with(['user', 'casts', 'nominees', 'tags'])
                     ->orderBy('date')
                     ->orderBy('start_time')->first();
+
+                if (!$order) {
+                    $order = Order::with('casts')
+                        ->where('user_id', Auth::user()->id)
+                        ->whereIn('status', [OrderStatus::OPEN, OrderStatus::ACTIVE])
+                        ->with(['user', 'casts', 'nominees', 'tags'])
+                        ->orderBy('date')
+                        ->orderBy('start_time')->first();
+                }
+
+                $order = OrderResource::make($order);
+
+                $client = new Client(['base_uri' => config('common.api_url')]);
+                $option = [
+                    'headers' => ['Authorization' => 'Bearer ' . $token],
+                    'form_params' => [],
+                    'allow_redirects' => false,
+                ];
+
+                $response = $client->get(route('casts.index', ['working_today' => 1, 'device' => 3]), $option);
+                $getContents = json_decode($response->getBody()->getContents());
+                $casts = $getContents->data;
+
+                return view('web.index', compact('token', 'order', 'casts'));
             }
 
-            $order = OrderResource::make($order);
+            if (UserType::CAST == $user->type) {
+                return redirect()->route('web.cast_index');
+            }
+        }
 
-            $client = new Client(['base_uri' => config('common.api_url')]);
-            $option = [
-                'headers' => ['Authorization' => 'Bearer ' . $token],
-                'form_params' => [],
-                'allow_redirects' => false,
-            ];
+        return redirect()->route('web.login');
+    }
 
-            $response = $client->get(route('casts.index', ['working_today' => 1, 'device' => 3]), $option);
-            $getContents = json_decode($response->getBody()->getContents());
-            $casts = $getContents->data;
+    public function castMypage()
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $token = '';
+            $token = JWTAuth::fromUser($user);
 
-            return view('web.index', compact('token', 'order', 'casts'));
+            if (UserType::CAST == $user->type) {
+                return view('web.cast.index', compact('token', 'user'));
+            } else {
+                return redirect()->route('web.login');
+            }
         }
 
         return redirect()->route('web.login');
