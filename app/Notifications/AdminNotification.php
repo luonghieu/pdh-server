6fc4f8f879afaf22234fe2532cba39faebfd3e53
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-11-15 16:39:11 +0700

    feature/MIK-1065 update send push logic

--- app/Notifications/AdminNotification.php
+++ app/Notifications/AdminNotification.php
@@ -51,6 +51,14 @@ class AdminNotification extends Notification implements ShouldQueue
                 return [CustomDatabaseChannel::class, LineBotNotificationChannel::class];
             }
 
+            if ($notifiable->device_type == DeviceType::IOS && $this->schedule->send_to != NotificationScheduleSendTo::IOS) {
+                return [];
+            }
+
+            if ($notifiable->device_type == DeviceType::ANDROID && $this->schedule->send_to != NotificationScheduleSendTo::ANDROID) {
+                return [];
+            }
+
             return [CustomDatabaseChannel::class, PushNotificationChannel::class];
         } else {
             if ($notifiable->device_type != DeviceType::WEB && $this->schedule->send_to != NotificationScheduleSendTo::WEB) {
