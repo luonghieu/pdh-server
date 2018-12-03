a95360f0efe9bd2f67b19670a3a0f3ad5946d121
Huynh Thi Hoai Thuong <thuong.hth@neo-lab.vn>
2018-12-03 11:25:15 +0700

    [MIK-1272] [Admin][A6 List orders]: Order detail in call/nominated call type cannot access with deleted user

--- app/Order.php
+++ app/Order.php
@@ -49,7 +49,7 @@ class Order extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function casts()
