18ae096000e00a11063cab9491af7f10be2c5cc8
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2018-12-06 15:56:03 +0700

    feature/MIK-1296 apply message for marketing in december

--- app/Notifications/MarketingOperation.php
+++ app/Notifications/MarketingOperation.php
@@ -12,6 +12,8 @@ class MarketingOperation extends Notification implements ShouldQueue
 {
     use Queueable;
 
+    public $isDayFive;
+
     /**
      * Create a new notification instance.
      *
@@ -35,6 +37,7 @@ class MarketingOperation extends Notification implements ShouldQueue
         $dayFive = $createdAt->copy()->addDays(5)->startOfDay();
         $daySix = $createdAt->copy()->addDays(6)->endOfDay();
         if ($now->between($dayFive, $daySix)) {
+            $this->isDayFive = $now->between($dayFive, $dayFive->copy()->endOfDay()) ? true : false;
             return [LineBotNotificationChannel::class];
         }
 
@@ -56,15 +59,28 @@ class MarketingOperation extends Notification implements ShouldQueue
             Storage::put($fileName, $fileContents, 'public');
         }
 
-        $message = '【新規ユーザー様限定！ギャラ飲み1時間無料🥂💕】'
-            . PHP_EOL . PHP_EOL . 'Cheersにご登録いただいてから1週間以内のゲスト様限定で、1時間無料キャンペーンを実施中！✨'
-            . PHP_EOL . PHP_EOL . '※予約方法は、コール予約、指名予約問いません。'
-            . PHP_EOL . '2時間以上のご予約で1時間無料となります（最大11,000円OFF）'
-            . PHP_EOL . PHP_EOL . 'ギャラ飲み初めての方も安心！'
-            . PHP_EOL . 'Cheersのキャストが盛り上げます🙋‍♀️❤️'
-            . PHP_EOL . '忘年会の季節に、キャストを呼んで飲み会や接待を盛り上げませんか？'
-            . PHP_EOL . PHP_EOL . 'ご登録から1週間を超えてしまうとキャンペーン対象外となりますのでお早めにご予約ください。'
-            . PHP_EOL . PHP_EOL . 'ご不明点はメッセージ内の運営者チャットからご連絡ください！';
+        if ($this->isDayFive) {
+            $message = '【キャンペーン終了まで残り2日😳💦】'
+                . PHP_EOL . 'Cheersにご登録いただいてから1週間以内のゲスト様限定で、1時間無料キャンペーンを実施中！✨'
+                . PHP_EOL . PHP_EOL . '※予約方法は、コール予約、指名予約問いません。'
+                . PHP_EOL . '2時間以上のご予約で1時間無料となります（最大11,000円OFF）'
+                . PHP_EOL . PHP_EOL . '忘年会の季節に、キャストを呼んで飲み会や接待を盛り上げませんか？'
+                . PHP_EOL . PHP_EOL . '無料体験ができるのは今だけ！'
+                . PHP_EOL . 'この機会にぜひご利用ください🌷'
+                . PHP_EOL . 'キャンペーン中のため利用者が増えております。'
+                . PHP_EOL . 'お早めのご予約がおすすめです！🙋‍♀️🌷'
+                . PHP_EOL . PHP_EOL . '残り2日でキャンペーン対象外となりますのでお早めにご予約ください。'
+                . PHP_EOL . PHP_EOL . 'ご不明点はメッセージ内の運営者チャットからご連絡ください！';
+        } else {
+            $message = '【本日最終日😣💦1時間無料キャンペーン】'
+                . PHP_EOL . 'Cheersにご登録いただいてから1週間以内のゲスト様限定で、1時間無料キャンペーンを実施中です！✨'
+                . PHP_EOL . PHP_EOL . '※予約方法は、コール予約、指名予約問いません。'
+                . PHP_EOL . '2時間以上のご予約で1時間無料となります（※最大11,000円OFF）'
+                . PHP_EOL . PHP_EOL . '予約時間が1分でも本日にかかっていれば、キャンペーン対象となります！🙌💓'
+                . PHP_EOL . PHP_EOL . 'この最後のチャンスをお見逃しなく！'
+                . PHP_EOL . PHP_EOL . 'ご不明点はメッセージ内の運営者チャットからご連絡ください！';
+        }
+
 
         return  [
             [
