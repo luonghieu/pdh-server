7dc336bd82b21241834d3ffb9d564b9f29cd9f59
Loan Van Thi (Studio 7 - PHP) <loan.vt@neo-lab.vn>
2018-12-06 15:35:31 +0700

    [MIK-1299][LP/Web] Show campaign banner and noticing text

--- resources/views/web/users/popup.blade.php
+++ resources/views/web/users/popup.blade.php
@@ -1,13 +1,13 @@
-<div class="modal_wrap">
+<div class="modal_wrap" id="hide-campaign">
   <input id="campaign" type="checkbox">
-  <div class="modal_overlay">
+  <div class="modal_overlay modal-close-campaign">
     <label for="campaign" class="modal_trigger"></label>
     <div class="modal_content">
       <div class="wrap-banner">
         <a href="{{ route('guest.orders.call') }}">
-          <img src="/assets/web/images/campaign/campaign_banner.png" alt="" class="banner">
+          <img src="/assets/web/images/campaign/campaign_12.png" alt="" class="banner">
         </a>
-        <label for="campaign"><img src="/assets/web/images/campaign/close.png" alt="" class="icon-close-banner"></label>
+        <label for="campaign"><img src="/assets/web/images/campaign/close.png" alt="" class="icon-close-banner" id="close-campaign"></label>
       </div>
     </div>
   </div>
