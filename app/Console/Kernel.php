44c15719bb6696e331f2802808613bf5d748d262
Bang Hoang <bang.hh@neo-lab.vn>
2018-12-07 14:11:03 +0700

    Merge code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -55,7 +55,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('cheers:delete_unused_point_after_180_days')->hourly()->onOneServer()->runInBackground();
         $schedule->command('cheers:inactive_chatroom_when_order_canceled')->hourlyAt(5)->onOneServer()->runInBackground();
         $schedule->command('cheers:notification_schedules')->everyMinute()->onOneServer()->runInBackground();
-        $schedule->command('cheers:marketing')->dailyAt('12:00')->when(function() {
+        $schedule->command('cheers:marketing')->dailyAt(env('CAMPAIGN_SEND_TIME'))->when(function () {
             $now = Carbon::now();
             $startDate = Carbon::parse(env('CAMPAIGN_FROM'))->startOfDay();
             $endDate = Carbon::parse(env('CAMPAIGN_TO'))->endOfDay();
