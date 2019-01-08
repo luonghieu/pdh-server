5899f8a4a8b13863a7a4aaee72d1e0aa024e3907
Pham Dinh Hung (MI2) <hung.pd@neo-lab.vn>
2019-01-08 10:45:30 +0700

    [MIK-1346] Refactor code

--- resources/assets/js/web/pages/order_step_three.js
+++ resources/assets/js/web/pages/order_step_three.js
@@ -1,46 +1,22 @@
-$(document).ready(function () {
-  const helper = require('./helper');
-  //step3 select casts 
-  function handelSelectedCasts()
-  {
-    $('#list-cast-order').on("change", ".cast_block .select-casts", function(event){
-      var id = $(this).val();
-      var countIds = JSON.parse(localStorage.getItem("order_call")).countIds;
-      if($('.select-casts:checked').length > countIds) {
-        var text = ' 指名できるキャストは'+ countIds + '名です';
-        $('#content-message h2').text(text);
-        $('#max-cast').prop('checked', true);
-        $(this).prop('checked',false);
-      }else {
-        if ($(this).is(':checked')) {
-          if(localStorage.getItem("order_call")){
-            var arrIds = JSON.parse(localStorage.getItem("order_call")).arrIds;
-            if (arrIds) {
-              if(arrIds.length < countIds) {
-                arrIds.push(id);
-                var params = {
-                    arrIds: arrIds
-                  };
+const helper = require('./helper');
 
-                $(this).prop('checked',true);
-                $(this).parent().find('.cast-link').addClass('cast-detail');
-                $('.label-select-casts[for='+  id  +']').text('リクエスト中');
-              } else {
-                var text = ' 指名できるキャストは'+ countIds + '名です';
-                $('#content-message h2').text(text);
-                $('#max-cast').prop('checked', true);
-                $(this).prop('checked',false);
-              }
-
-              if(arrIds.length) {
-                $('#sb-select-casts a').text('次に進む(3/4)');
-              } else {
-                $('#sb-select-casts a').text('希望リクエストせずに進む(3/4)');
-              }
-
-            } else {
-              var arrIds = [id];
-
+function handelSelectedCasts()
+{
+  $('#list-cast-order').on("change", ".cast_block .select-casts", function(event){
+    var id = $(this).val();
+    var countIds = JSON.parse(localStorage.getItem("order_call")).countIds;
+    if($('.select-casts:checked').length > countIds) {
+      var text = ' 指名できるキャストは'+ countIds + '名です';
+      $('#content-message h2').text(text);
+      $('#max-cast').prop('checked', true);
+      $(this).prop('checked',false);
+    }else {
+      if ($(this).is(':checked')) {
+        if(localStorage.getItem("order_call")){
+          var arrIds = JSON.parse(localStorage.getItem("order_call")).arrIds;
+          if (arrIds) {
+            if(arrIds.length < countIds) {
+              arrIds.push(id);
               var params = {
                   arrIds: arrIds
                 };
@@ -48,81 +24,105 @@ $(document).ready(function () {
               $(this).prop('checked',true);
               $(this).parent().find('.cast-link').addClass('cast-detail');
               $('.label-select-casts[for='+  id  +']').text('リクエスト中');
-              $('#sb-select-casts a').text('次に進む(3/4)');
+            } else {
+              var text = ' 指名できるキャストは'+ countIds + '名です';
+              $('#content-message h2').text(text);
+              $('#max-cast').prop('checked', true);
+              $(this).prop('checked',false);
             }
+
+            if(arrIds.length) {
+              $('#sb-select-casts a').text('次に進む(3/4)');
+            } else {
+              $('#sb-select-casts a').text('希望リクエストせずに進む(3/4)');
+            }
+
           } else {
             var arrIds = [id];
 
             var params = {
                 arrIds: arrIds
               };
+
+            $(this).prop('checked',true);
+            $(this).parent().find('.cast-link').addClass('cast-detail');
+            $('.label-select-casts[for='+  id  +']').text('リクエスト中');
+            $('#sb-select-casts a').text('次に進む(3/4)');
           }
         } else {
-          if(localStorage.getItem("order_call")){
-            var arrIds = JSON.parse(localStorage.getItem("order_call")).arrIds;
-            if(arrIds) {
-              if(arrIds.indexOf(id) > -1) {
-                arrIds.splice(arrIds.indexOf(id), 1);
-              }
-
-              var params = {
-                arrIds: arrIds,
-              }
-
-              if(arrIds.length) {
-                $('#sb-select-casts a').text('次に進む(3/4)');
-              } else {
-                $('#sb-select-casts a').text('希望リクエストせずに進む(3/4)');
-              }
-            }
-          }
-
-          $(this).prop('checked',false);
-          $(this).parent().find('.cast-link').removeClass('cast-detail');
-          $('.label-select-casts[for='+  id  +']').text('リクエストする');
-        }
-      }
-
-      if(params) {
-        helper.updateLocalStorageValue('order_call', params);
-        $(".cast-ids").val(arrIds.toString());
-      }
-    });
-
-    $("#cast-order-call a").on("click",function(event){
-      var id = $('#cast-id-info').val();
-      if(localStorage.getItem("order_call")){
-        var arrIds = JSON.parse(localStorage.getItem("order_call")).arrIds;
-        var countIds = JSON.parse(localStorage.getItem("order_call")).countIds;
-        if(arrIds) {
-          if(arrIds.length < countIds) {
-            if(arrIds.indexOf(id) < 0) {
-              arrIds.push(id);
-
-              var params = {
-                arrIds: arrIds,
-              };
-            }
-          } else {
-            if(arrIds.indexOf(id) < 0) {
-              localStorage.setItem('full',true);
-            }
-          }
-        } else {
-          var arrIds = [];
-          arrIds.push(id);
+          var arrIds = [id];
 
           var params = {
               arrIds: arrIds
             };
         }
-      }
+      } else {
+        if(localStorage.getItem("order_call")){
+          var arrIds = JSON.parse(localStorage.getItem("order_call")).arrIds;
+          if(arrIds) {
+            if(arrIds.indexOf(id) > -1) {
+              arrIds.splice(arrIds.indexOf(id), 1);
+            }
 
-      if(params) {
-        helper.updateLocalStorageValue('order_call', params);
+            var params = {
+              arrIds: arrIds,
+            }
+
+            if(arrIds.length) {
+              $('#sb-select-casts a').text('次に進む(3/4)');
+            } else {
+              $('#sb-select-casts a').text('希望リクエストせずに進む(3/4)');
+            }
+          }
+        }
+
+        $(this).prop('checked',false);
+        $(this).parent().find('.cast-link').removeClass('cast-detail');
+        $('.label-select-casts[for='+  id  +']').text('リクエストする');
       }
-    })
-  }
-  
+    }
+
+    if(params) {
+      helper.updateLocalStorageValue('order_call', params);
+      $(".cast-ids").val(arrIds.toString());
+    }
+  });
+
+  $("#cast-order-call a").on("click",function(event){
+    var id = $('#cast-id-info').val();
+    if(localStorage.getItem("order_call")){
+      var arrIds = JSON.parse(localStorage.getItem("order_call")).arrIds;
+      var countIds = JSON.parse(localStorage.getItem("order_call")).countIds;
+      if(arrIds) {
+        if(arrIds.length < countIds) {
+          if(arrIds.indexOf(id) < 0) {
+            arrIds.push(id);
+
+            var params = {
+              arrIds: arrIds,
+            };
+          }
+        } else {
+          if(arrIds.indexOf(id) < 0) {
+            localStorage.setItem('full',true);
+          }
+        }
+      } else {
+        var arrIds = [];
+        arrIds.push(id);
+
+        var params = {
+            arrIds: arrIds
+          };
+      }
+    }
+
+    if(params) {
+      helper.updateLocalStorageValue('order_call', params);
+    }
+  })
+}
+
+$(document).ready(function () {
   handelSelectedCasts();
 });
--- resources/assets/js/web/pages/order_step_two.js
+++ resources/assets/js/web/pages/order_step_two.js
@@ -1,66 +1,66 @@
-$(document).ready(function () {
-  const helper = require('./helper');
-  //step2 select tags
-  function handelSelectedTags()
-  {
-    $(".form-grpup .checkbox-tags").on("change",function(event){
-      var tagName = $(this).children().val();
-      var activeSum = $(".active").length;
-      if ($(this).hasClass("active")) {
+const helper = require('./helper');
+
+function handelSelectedTags()
+{
+  $(".form-grpup .checkbox-tags").on("change",function(event){
+    var tagName = $(this).children().val();
+    var activeSum = $(".active").length;
+    if ($(this).hasClass("active")) {
+      $(this).children().prop('checked',false);
+      $(this).removeClass('active');
+
+      if(localStorage.getItem("order_call")){
+        var tags = JSON.parse(localStorage.getItem("order_call")).tags;
+        if(tags) {
+          if(tags.indexOf(tagName) > -1) {
+            tags.splice(tags.indexOf(tagName), 1);
+          }
+
+          var params = {
+            tags: tags,
+          };
+
+        }
+      }
+    } else {
+      if(activeSum >= 5) {
+        $('#max-tags').prop('checked', true);
         $(this).children().prop('checked',false);
         $(this).removeClass('active');
+      } else {
+        $(this).children().prop('checked',true);
+        $(this).addClass('active');
 
         if(localStorage.getItem("order_call")){
           var tags = JSON.parse(localStorage.getItem("order_call")).tags;
           if(tags) {
-            if(tags.indexOf(tagName) > -1) {
-              tags.splice(tags.indexOf(tagName), 1);
-            }
+            tags.push(tagName);
 
             var params = {
               tags: tags,
             };
-
-          }
-        }
-      } else {
-        if(activeSum >= 5) {
-          $('#max-tags').prop('checked', true);
-          $(this).children().prop('checked',false);
-          $(this).removeClass('active');
-        } else {
-          $(this).children().prop('checked',true);
-          $(this).addClass('active');
-
-          if(localStorage.getItem("order_call")){
-            var tags = JSON.parse(localStorage.getItem("order_call")).tags;
-            if(tags) {
-              tags.push(tagName);
-
-              var params = {
-                tags: tags,
-              };
-              
-            } else {
-              var tags = [tagName];
-              var params = {
-                tags: tags,
-              };
-            }
+            
           } else {
             var tags = [tagName];
             var params = {
-                tags: tags,
-              };
+              tags: tags,
+            };
           }
+        } else {
+          var tags = [tagName];
+          var params = {
+              tags: tags,
+            };
         }
       }
+    }
 
-      if(params) {
-        helper.updateLocalStorageValue('order_call', params);
-      }
-    });
-  }
+    if(params) {
+      helper.updateLocalStorageValue('order_call', params);
+    }
+  });
+}
 
+$(document).ready(function () {
   handelSelectedTags();
 });
