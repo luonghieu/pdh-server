7fc52445d32a55ba9611b906f8a5fe6206e3f9e7
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-01-15 15:27:32 +0700

    feature/MIk-1417 update notification messages

--- app/Notifications/PaymentRequestFromCast.php
+++ app/Notifications/PaymentRequestFromCast.php
@@ -110,7 +110,7 @@ class PaymentRequestFromCast extends Notification implements ShouldQueue
                 . PHP_EOL . '※詳細に誤りがある場合は、3時間以内に「決済ポイントの修正依頼をする」を押してください。'
                 . PHP_EOL . '延長料金が' . number_format($totalPoint) .'Point発生しておりますので、別途運営から決済画面をお送りいたします。';
         } else {
-            $content = 'Cheersをご利用いただきありがとうございました♪' . $orderStartDate->format('Y/m/d H:i') . 'のご利用ポイント、15,000Point 
+            $content = 'Cheersをご利用いただきありがとうございました♪' . $orderStartDate->format('Y/m/d H:i') . 'のご利用ポイント、' . number_format($totalPoint) .'Point 
             のご清算が完了いたしました。' . ' マイページの「ポイント履歴」から領収書の発行が可能です。' . $guestNickname . 'のまたのご利用をお待ちしております♪';
         }
 
