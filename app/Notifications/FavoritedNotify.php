9daefcef806b78bd49af5f7a45ec8d6d8d0b29d6
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-02-11 15:19:36 +0700

    feature/MIK-1468 send like img when user got favourited

--- app/Notifications/FavoritedNotify.php
+++ app/Notifications/FavoritedNotify.php
@@ -3,15 +3,19 @@
 namespace App\Notifications;
 
 use App\Enums\DeviceType;
+use App\Enums\MessageType;
 use App\Enums\ProviderType;
+use App\Enums\SystemMessageType;
 use App\Enums\UserType;
+use App\Traits\DirectRoom;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Storage;
 
 class FavoritedNotify extends Notification implements ShouldQueue
 {
-    use Queueable;
+    use Queueable, DirectRoom;
 
     public $user;
 
@@ -54,6 +58,22 @@ class FavoritedNotify extends Notification implements ShouldQueue
 
     public function pushData($notifiable)
     {
+        $room = $this->createDirectRoom($notifiable->id, $this->user->id);
+        $likeImgSrc = Storage::url('iine3.png');;
+        if (!@getimagesize($likeImgSrc)) {
+            $fileContents = Storage::disk('local')->get("system_images/iine3.png");
+            $fileName = 'iine3.png';
+            Storage::put($fileName, $fileContents, 'public');
+        }
+        $likeImgMessge = $room->messages()->create([
+            'user_id' => 1,
+            'type' => MessageType::IMAGE,
+            'image' => 'iine3.png',
+            'system_type' => SystemMessageType::NORMAL,
+            'created_at' => now()->copy()->addSeconds(2)
+        ]);
+        $likeImgMessge->recipients()->attach($notifiable->id, ['room_id' => $room->id]);
+
         $content = $this->user->nickname . 'さんからイイネされました！';
         $namedUser = 'user_' . $notifiable->id;
         $send_from = UserType::ADMIN;
