e569c6de6a95b9336f222b7c7cf05fb6ba629612
Huynh Thi Hoai Thuong <thuong.hth@neo-lab.vn>
2019-02-13 09:35:51 +0700

    [MIK-1509] Update migrate

--- database/migrations/2019_02_13_105626_create_rank_schedules_table.php
+++ database/migrations/2019_02_13_105626_create_rank_schedules_table.php
@@ -18,9 +18,9 @@ class CreateRankSchedulesTable extends Migration
             $table->date('from_date')->nullable();
             $table->date('to_date')->nullable();
             $table->tinyInteger('num_of_attend_platium')->nullable();
-            $table->float('num_of_avg_rate_platium')->default(0.0);
+            $table->float('num_of_avg_rate_platium')->nullable();
             $table->tinyInteger('num_of_attend_up_platium')->nullable();
-            $table->float('num_of_avg_rate_up_platium')->default(0.0);
+            $table->float('num_of_avg_rate_up_platium')->nullable();
             $table->timestamps();
         });
     }
