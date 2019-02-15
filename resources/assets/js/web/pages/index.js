c5856e52165a6c06ff18b9d56c0336f658cefafe
Huynh Thi Hoai Thuong <thuong.hth@neo-lab.vn>
2019-02-15 10:17:49 +0700

    [MIK-1528] Update load cast working today when load page

--- resources/assets/js/web/pages/index.js
+++ resources/assets/js/web/pages/index.js
@@ -25,16 +25,31 @@ $(document).ready(function() {
   //save prefecture in my page
 
   if ($('#prefecture-id-mypage').length) {
+    var prefectureId = $('#prefecture-id-mypage').val();
 
     if (localStorage.getItem("prefecture_id")) {
       $('#prefecture-id-mypage').val(localStorage.getItem("prefecture_id"));
+      prefectureId = localStorage.getItem("prefecture_id");
+
+      // display cast working today in mypape
+      var params = {
+        prefecture_id: prefectureId,
+        working_today: 1,
+        response_type: 'html'
+      };
+
+      window.axios.get('/api/v1/casts', {params})
+        .then(function(response) {
+          $('.cast-body').html(response['data']);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
 
     if (!localStorage.getItem("prefecture_id")) {
-      var prefectureId = $('#prefecture-id-mypage').val();
       localStorage.setItem('prefecture_id', prefectureId);
     }
-
   }
 
   $('#prefecture-id-mypage').on('change', function () {
@@ -57,7 +72,4 @@ $(document).ready(function() {
         console.log(error);
       });
   });
-
-
-
 });
