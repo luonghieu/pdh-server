d7e9ce719fc6a596fee7267084b4e3e457345964
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-02-20 16:16:35 +0700

    feature/MIK-1468 add message like

--- app/Notifications/FavoritedNotify.php
+++ app/Notifications/FavoritedNotify.php
@@ -112,6 +112,22 @@ class FavoritedNotify extends Notification implements ShouldQueue
 
     public function lineBotPushData($notifiable)
     {
+        $room = $this->createDirectRoom($notifiable->id, $this->user->id);
+        $likeImgSrc = Storage::url('iine3.png');;
+        if (!@getimagesize($likeImgSrc)) {
+            $fileContents = Storage::disk('local')->get("system_images/iine3.png");
+            $fileName = 'iine3.png';
+            Storage::put($fileName, $fileContents, 'public');
+        }
+        $likeImgMessge = $room->messages()->create([
+            'user_id' => $this->user->id,
+            'type' => MessageType::LIKE,
+            'image' => 'iine3.png',
+            'system_type' => SystemMessageType::NORMAL,
+            'created_at' => now()->copy()->addSeconds(2)
+        ]);
+        $likeImgMessge->recipients()->attach($notifiable->id, ['room_id' => $room->id]);
+
         $content = $this->user->nickname . 'さんからイイネされました！';
         $page = env('LINE_LIFF_REDIRECT_PAGE') . '?page=cast&cast_id=' . $this->user->id;
 
