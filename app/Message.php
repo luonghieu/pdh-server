e8a3eabb272c150aaed5a9ce16de159b0dd8bc6f
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-02-21 15:48:56 +0700

    feature/MIK-1559 update model message add trash user

--- app/Message.php
+++ app/Message.php
@@ -64,7 +64,7 @@ class Message extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function recipients()
