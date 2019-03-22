8415f2e091db8b8376d19a89f566587561983333
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-03-22 08:47:46 +0700

    feature/invite-code add delay when send push invite point

--- app/Observers/InviteCodeHistoryObserver.php
+++ app/Observers/InviteCodeHistoryObserver.php
@@ -11,12 +11,13 @@ class InviteCodeHistoryObserver
     public function updated(InviteCodeHistory $inviteCodeHistory)
     {
         if ($inviteCodeHistory->getOriginal('status') != $inviteCodeHistory->status && $inviteCodeHistory->status == InviteCodeHistoryStatus::RECEIVED) {
+            $now = now()->addSeconds(3);
             $userInvite = $inviteCodeHistory->inviteCode->user;
-            $userInvite->notify(new AddedInvitePoint());
+            $userInvite->notify((new AddedInvitePoint())->delay($now));
 
             $order = $inviteCodeHistory->order;
             $user = $order->user;
-            $user->notify(new AddedInvitePoint(true));
+            $user->notify((new AddedInvitePoint(true))->delay($now));
         }
     }
 }
