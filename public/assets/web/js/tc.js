b1149b1ee10d75e2b9679380eaa8bd728dde2104
Bang Hoang <bang.hh@neo-lab.vn>
2019-03-26 10:41:26 +0700

    [MIK-1579] Showing logo

--- public/assets/web/js/tc.js
+++ public/assets/web/js/tc.js
@@ -7,6 +7,19 @@ $(document).ready(function() {
     $('.btn-back.header-item a').attr('href', 'cheers://back');
   }
 
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  var mypage = $('.h-logo a').attr('href');
+
+  $('.h-logo a').click(function (e) {
+    e.preventDefault();
+
+    if (/android/i.test(userAgent)) {
+      window.location = 'cheers://back';
+    } else {
+      window.location = mypage;
+    }
+  });
+
   $('#year-select')
   .find('option')
   .each(function (index, value) {
