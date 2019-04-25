f4948fca8c97df8a4e9f7d4fe3d7db697b4bb330
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-04-25 10:46:58 +0700

    temp

---
+++ app/TimeLine.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TimeLine extends Model
+{
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(TimeLineFavorite::class);
+    }
+}
---
+++ database/migrations/2019_04_25_121714_create_time_lines_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_lines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image');
+            $table->string('location');
+            $table->boolean('hidden')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_lines');
+    }
+}
