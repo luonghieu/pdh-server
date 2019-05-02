5878a2a5314d06012697f5663fa6e072e2660c90
Pham Dinh Hung <hung.pd@neo-lab.vn>
2019-05-02 10:12:03 +0700

    Create api for time_lines table

---
+++ app/Http/Controllers/Api/TimeLineController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\TimeLineResource;
+use App\TimeLine;
+use Illuminate\Http\Request;
+
+class TimeLineController extends ApiController
+{
+    public function index(Request $request)
+    {
+        $user = $this->guard()->user();
+
+        $id = $user->id;
+        if ($request->user_id) {
+            $id = $request->user_id;
+        }
+
+        $timeLine = TimeLine::where('user_id', $id)->where('hidden', false)->with('favorites')->paginate(10);
+
+        return $this->respondWithData(TimeLineResource::collection($timeLine));
+    }
+}
---
+++ app/Http/Resources/TimeLineResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Traits\ResourceResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TimeLineResource extends JsonResource
+{
+    use ResourceResponse;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return $this->filterNull([
+            'id' => $this->id,
+            'user' => new UserResource($this->user),
+            'title' => $this->title,
+            'content' => $this->content,
+            'image' => $this->image,
+            'location' => $this->location,
+            'total_favorites' => count($this->whenLoaded('favorites')),
+            'hidden' => $this->hidden,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ]);
+    }
+}
