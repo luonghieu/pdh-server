838d499c19f67197765e492f4984a416eb1667d1
Ngo Ngoc Viet <viet.nn@neo-lab.vn>
2019-05-06 09:21:15 +0700

    feature/timeline add with trash for user model

--- app/TimeLineFavorite.php
+++ app/TimeLineFavorite.php
@@ -14,6 +14,6 @@ class TimeLineFavorite extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
