09f5722a73647c42be0813c7dc40704593c1ee74
Loan Van Thi (Studio 7 - PHP) <loan.vt@neo-lab.vn>
2019-05-13 10:37:15 +0700

    [MIK-1854][Web][GF-2] Guest can confirm tweet of cast on cast detail

--- resources/views/web/users/show.blade.php
+++ resources/views/web/users/show.blade.php
@@ -205,168 +205,57 @@
       <h2 class="portlet-header__title title-shifts">タイムライン</h2>
     </div>
     <div class="portlet-content--timeline">
+      @foreach($timelines as $timeline)
       <div class="timeline-list">
         <div class="timeline-item">
           <div class="user-info">
             <div class="user-info__profile">
-              <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg" alt="">
+              <img src="{{ $timeline['user']['avatars'][0]['path'] }}" alt="">
             </div>
             <div class="user-info__text">
               <div class="user-info__top">
-                <p>Ayaka</p>
-                <p>22</p>
+                <p>{{ $timeline['user']['nickname'] }}</p>
+                <p>{{ $timeline['user']['age'] }}</p>
               </div>
               <div class="user-info__bottom">
-                <p>新宿</p>
-                <p>16時間前</p>
+                <p>{{ $timeline['location'] ?? '' }}</p>
+                <p>{{ latestOnlineStatus($timeline['created_at']) }}</p>
               </div>
             </div>
           </div>
           <div class="timeline-content">
-            <div class="timeline-article">
-              <div class="timeline-article__text">今日今から一緒に飲めるかた！
-                <br>可愛いまどかちゃんと一緒にいます❤︎
-                <br>親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした 事がある。</div>
-            </div>
-            <div class="timeline-images">
-              <div class="timeline-images__list">
-                <div class="timeline-images__item">
-                  <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg">
+            <a href="{{ route('web.timelines.show', ['timeline' => $timeline['id']]) }}" class="init-text-color">
+              <div class="timeline-article">
+                <div class="timeline-article__text">
+                  {!! $timeline['content'] !!}
                 </div>
               </div>
-            </div>
-            <div class="timeline-like">
-              <button class="timeline-like__icon">
-                <img src="{{ asset('assets/web/images/common/like-icon_on.svg ') }}" alt="">
-              </button>
-              <p class="timeline-like__sum">113</p>
-            </div>
-          </div>
-        </div>
-        <div class="timeline-item">
-          <div class="user-info">
-            <div class="user-info__profile">
-              <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg" alt="">
-            </div>
-            <div class="user-info__text">
-              <div class="user-info__top">
-                <p>Ayaka</p>
-                <p>22</p>
-              </div>
-              <div class="user-info__bottom">
-                <p>新宿</p>
-                <p>3日</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-content">
-            <div class="timeline-article">
-              <div class="timeline-article__text">一緒に楽しく飲みましょう！と思える人がいいな-</div>
-            </div>
-            <div class="timeline-like">
-              <button class="timeline-like__icon">
-                <img src="{{ asset('assets/web/images/common/like-icon_on.svg ') }}" alt="">
-              </button>
-              <p class="timeline-like__sum">54</p>
-            </div>
-          </div>
-        </div>
-        <div class="timeline-item">
-          <div class="user-info">
-            <div class="user-info__profile">
-              <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg" alt="">
-            </div>
-            <div class="user-info__text">
-              <div class="user-info__top">
-                <p>カトリーヌまどか@今日は気分がいい…</p>
-                <p>22歳</p>
-              </div>
-              <div class="user-info__bottom">
-                <p>新宿</p>
-                <p>4月2日</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-content">
-            <div class="timeline-article">
-              <div class="timeline-article__text">今日今から一緒に飲めるかた！
-                <br>可愛いまどかちゃんと一緒にいます❤︎
-                <br>親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした 事がある。</div>
-            </div>
-            <div class="timeline-images">
-              <div class="timeline-images__list">
-                <div class="timeline-images__item">
-                  <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg">
+              <div class="timeline-images">
+                <div class="timeline-images__list">
+                  <div class="timeline-images__item">
+                    <img src="{{ $timeline['image'] }}">
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="timeline-like">
+            </a>
+            <div class="timeline-like heart-timeline" data-timeline-id="{{ $timeline['id'] }}">
               <button class="timeline-like__icon">
-                <img src="{{ asset('assets/web/images/common/like-icon_on.svg ') }}" alt="">
+                <div id="heart-timeline-{{ $timeline['id'] }}" data-is-favorited-timeline="{{ $timeline['is_favourited'] }}" data-total-favorites-timeline="{{ $timeline['total_favorites'] }}">
+                  @if($timeline['is_favourited'])
+                  <img src="{{ asset('assets/web/images/common/like.svg ') }}" alt="">
+                  @else
+                  <img src="{{ asset('assets/web/images/common/unlike.svg ') }}" alt="">
+                  @endif
+                </div>
               </button>
-              <p class="timeline-like__sum">407</p>
-            </div>
-          </div>
-        </div>
-        <div class="timeline-item">
-          <div class="user-info">
-            <div class="user-info__profile">
-              <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg" alt="">
-            </div>
-            <div class="user-info__text">
-              <div class="user-info__top">
-                <p>Ayaka</p>
-                <p>22</p>
-              </div>
-              <div class="user-info__bottom">
-                <p>新宿</p>
-                <p>3日</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-content">
-            <div class="timeline-article">
-              <div class="timeline-article__text">一緒に楽しく飲みましょう！と思える人がいいな-</div>
-            </div>
-            <div class="timeline-like">
-              <button class="timeline-like__icon">
-                <img src="{{ asset('assets/web/images/common/like-icon_on.svg ') }}" alt="">
-              </button>
-              <p class="timeline-like__sum">54</p>
-            </div>
-          </div>
-        </div>
-        <div class="timeline-item">
-          <div class="user-info">
-            <div class="user-info__profile">
-              <img src="https://i.pinimg.com/originals/4f/32/90/4f329022530b11cbbec602b5288c21f8.jpg" alt="">
-            </div>
-            <div class="user-info__text">
-              <div class="user-info__top">
-                <p>Ayaka</p>
-                <p>22</p>
-              </div>
-              <div class="user-info__bottom">
-                <p>新宿</p>
-                <p>3日</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-content">
-            <div class="timeline-article">
-              <div class="timeline-article__text">一緒に楽しく飲みましょう！と思える人がいいな-</div>
-            </div>
-            <div class="timeline-like">
-              <button class="timeline-like__icon timeline-like__icon--clicked">
-                <img src="{{ asset('assets/web/images/common/like-icon.svg') }}" alt="">
-              </button>
-              <p class="timeline-like__sum">54</p>
+              <p class="timeline-like__sum" id="total-favorites-{{ $timeline['id'] }}">{{ $timeline['total_favorites'] }}</p>
             </div>
           </div>
         </div>
       </div>
+      @endforeach
       <div class="timeline-more">
-        <p>さらに見る</p>
+        <a href="{{ route('web.timelines.index') }}"><p>さらに見る</p></a>
       </div>
     </div>
   </section>
