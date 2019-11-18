0dcecafffcb2e728802033656235eb28ea9578fd
Loan Van Thi (Studio 7 - PHP) <loan.vt@neo-lab.vn>
2019-11-18 13:39:02 +0700

    [MIK-fix-logic-sub-point-when-adjusted] Change logic adjusted

--- app/Console/Commands/DeleteUnusedPointAfter180Days.php
+++ app/Console/Commands/DeleteUnusedPointAfter180Days.php
@@ -44,13 +44,20 @@ class DeleteUnusedPointAfter180Days extends Command
     public function handle()
     {
         $dateTime = Carbon::now()->subDays(180)->format('Y-m-d H');
-        $points = Point::whereIn('type', 
-            [
-                PointType::BUY, 
-                PointType::AUTO_CHARGE, 
-                PointType::INVITE_CODE, 
-                PointType::DIRECT_TRANSFER,
-            ])
+        $points = Point::where(function ($query) {
+            $query->whereIn('type',
+                [
+                    PointType::BUY,
+                    PointType::AUTO_CHARGE,
+                    PointType::INVITE_CODE, 
+                    PointType::DIRECT_TRANSFER,
+                ])
+                ->orWhere(function ($subQ) {
+                    $subQ->where('type', PointType::ADJUSTED)
+                        ->where('is_cast_adjusted', false)
+                        ->where('point', '>=', 0);
+                });
+            })
             ->where(\DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d %H") '), '<=', $dateTime)
             ->where('balance', '>', 0);
 
@@ -61,7 +68,7 @@ class DeleteUnusedPointAfter180Days extends Command
                 $balancePoint = $point->balance;
                 $data = [
                     'point' => -$balancePoint,
-                    'balance' => $point->user->point - $balancePoint,
+                    'balance' => $point->user ? ($point->user->point - $balancePoint) : 0,
                     'user_id' => $point->user_id,
                     'type' => PointType::EVICT,
                 ];
@@ -95,9 +102,11 @@ class DeleteUnusedPointAfter180Days extends Command
                 $point->balance = 0;
                 $point->save();
 
-                $user = User::withTrashed()->find($point->user->id);
-                $user->point -= $balancePoint;
-                $user->save();
+                if ($point->user) {
+                    $user = User::withTrashed()->find($point->user->id);
+                    $user->point -= $balancePoint;
+                    $user->save();
+                }
 
                 DB::commit();
             }
