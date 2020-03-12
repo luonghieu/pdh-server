1cad5f5d03be2e6e73d11c5d11a4b28987533356
Loan Van Thi (Studio 7 - PHP) <loan.vt@neo-lab.vn>
2020-03-12 20:40:09 +0700

    [MIK-150-working-today] Fix bug: unable to change status working today

--- app/Http/Controllers/Api/WorkingTodayController.php
+++ app/Http/Controllers/Api/WorkingTodayController.php
@@ -16,15 +16,17 @@ class WorkingTodayController extends ApiController
         $today = Carbon::today();
         $user->working_today = !$user->working_today;
         $shiftToday = $user->shifts()->where('date', $today)->first();
-        if ($user->working_today) {
-            $shiftToday->pivot->day_shift = $user->working_today;
-            $shiftToday->pivot->off_shift = false;
-            $shiftToday->pivot->save();
-        } else {
-            $shiftToday->pivot->day_shift = $user->working_today;
-            $shiftToday->pivot->night_shift = $user->working_today;
-            $shiftToday->pivot->off_shift = true;
-            $shiftToday->pivot->save();
+        if ($shiftToday) {
+            if ($user->working_today) {
+                $shiftToday->pivot->day_shift = $user->working_today;
+                $shiftToday->pivot->off_shift = false;
+                $shiftToday->pivot->save();
+            } else {
+                $shiftToday->pivot->day_shift = $user->working_today;
+                $shiftToday->pivot->night_shift = $user->working_today;
+                $shiftToday->pivot->off_shift = true;
+                $shiftToday->pivot->save();
+            }
         }
 
         $user->update();
